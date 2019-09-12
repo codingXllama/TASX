ee7f54c93cdaa285a9fbaa106ed24ba0f4e2e203
codingxllama <osamaalchalabi2@gmail.com>
2019-09-12 01:42:36 -0400

    Settings up the database

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 # setting up the app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db '
+# initializing the db for our app settings
+db = SQLAlchemy(app)
 
 # routing the app
 @app.route('/')
