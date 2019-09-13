3b6c27ee92b1ba413051699455cae5175a3df59e
codingxllama <osamaalchalabi2@gmail.com>
2019-09-13 00:38:29 -0400

    Created the ability to add tasks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 # setting up the app
@@ -15,7 +15,7 @@ class Todo(db.Model):
     # Creating columns with id
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
+    # completed = db.Column(db.Integer, default=0)
     # setting the date time when a column is created
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -27,9 +27,30 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 # defining the route method
 def index():
-    # rendering the html file
+    if request.method == 'POST':
+        # Creating logic for adding tasks
+        task_content = request.form['content']
+        # Creating a new task(to do object) object using the Todo class
+        # Setting the content of the Todo list, by assigning the content instance variable to the new_task content
+        new_task = Todo(content=task_content)
 
-    return render_template('index.html')
+        # pushing the task to the database ~ we must use try block
+        try:
+            db.session.add(new_task)
+            # to execute the db
+            db.session.commit()
+            # redirecting back to the index page
+            return redirect('/')
+
+        except:
+            return 'There was an issue adding the task, please try again!'
+
+    else:
+        # looking at all the tasks created and arranging them by the order created
+        # use .first() to get first() created
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        # rendering the html file
+        return render_template('index.html', tasks=tasks)
 
 
 # Running the app
